d17fd26a1b390d1aa8bef1bbe81a59600d403886
daveycodez <david@swiftapk.com>
2025-01-30 21:41:52 -0800

    queries

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export * from "./lib/neon-query-provider"
-export * from "./lib/db-queries"
 export * from "./hooks/create-query-hooks"
 export { serializeConfig } from "./lib/utils"
---
+++ src/queries.ts
@@ -0,0 +1,2 @@
+export * from "./lib/prefetch-queries"
+export * from "./lib/db-queries"
