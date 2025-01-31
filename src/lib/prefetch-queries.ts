a2605d1bcc8aefd450697a91334851439a871b4c
daveycodez <david@swiftapk.com>
2025-01-30 22:02:38 -0800

    prefetchFindFirst

--- src/lib/prefetch-queries.ts
+++ src/lib/prefetch-queries.ts
@@ -1,8 +1,14 @@
 import { AnyUseQueryOptions, type QueryClient } from "@tanstack/react-query"
-import { type BuildQueryResult, type DBQueryConfig, type TablesRelationalConfig } from "drizzle-orm"
+import {
+    type BuildQueryResult,
+    type DBQueryConfig,
+    type TablesRelationalConfig,
+    eq,
+    sql
+} from "drizzle-orm"
 import { type PgDatabase, type PgQueryResultHKT } from "drizzle-orm/pg-core"
 
-import { findMany } from "./db-queries"
+import { findFirst, findMany } from "./db-queries"
 import { serializeConfig } from "./utils"
 
 export async function prefetchFindMany<
@@ -20,7 +26,7 @@ export async function prefetchFindMany<
 ) {
     type TableType = BuildQueryResult<TSchema, TSchema[TableName], TConfig>
 
-    if (process.env.NODE_ENV === "development") await new Promise((resolve) => setTimeout(resolve, 200))
+    if (process.env.NODE_ENV === "development") await new Promise((resolve) => setTimeout(resolve, 250))
 
     const queryKey = [table, "list", ...(config ? [serializeConfig(config)] : [])]
     const results = await findMany(db, table, config)
@@ -34,4 +40,41 @@ export async function prefetchFindMany<
     })
 
     return results as TableType[]
+}
+
+export async function prefetchFindFirst<
+    TQueryResult extends PgQueryResultHKT,
+    TFullSchema extends Record<string, unknown>,
+    TSchema extends TablesRelationalConfig,
+    TableName extends keyof TSchema,
+    TConfig extends DBQueryConfig<"one", true, TSchema, TSchema[TableName]>,
+    IDType = TSchema[TableName]["columns"]["id"]["_"]["data"]
+>(
+    db: PgDatabase<TQueryResult, TFullSchema, TSchema>,
+    queryClient: QueryClient,
+    table: TableName,
+    id?: IDType | null,
+    config?: TConfig | null,
+    options?: Omit<AnyUseQueryOptions, "queryKey" | "queryFn"> | null
+) {
+    type TableType = BuildQueryResult<TSchema, TSchema[TableName], TConfig>
+
+    if (process.env.NODE_ENV === "development") await new Promise((resolve) => setTimeout(resolve, 250))
+
+    const queryKey = [table, "detail", id, ...((!id && config) ? [serializeConfig(config)] : [])]
+
+    const result = await findFirst(db, table, {
+        where: id ? eq(sql`id`, id) : undefined,
+        ...config
+    })
+
+    queryClient.prefetchQuery<TableType>({
+        ...options,
+        queryKey,
+        queryFn: async () => {
+            return result as TableType
+        }
+    })
+
+    return result as TableType
 }
